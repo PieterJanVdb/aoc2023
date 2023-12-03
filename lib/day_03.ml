0fcf9f750df6d45de6e8e162a25c6cc69f2ffef6
Pieter-Jan Vandenbussche <pj.vandenbussche@gmail.com>
2023-12-03 23:06:50 +0100

    day 3, inline fns

--- lib/day_03.ml
+++ lib/day_03.ml
@@ -7,22 +7,20 @@ let get_digit lines (row, l, r) =
   List.nth_exn lines row |> String.sub ~pos:l ~len:(r - l + 1) |> Int.of_string
 ;;
 
-let get_line_locations re row line =
-  List.map (Re.all re line) ~f:(fun group ->
-    let l, r = (Re.Group.all_offset group).(0) in
-    row, l, r - 1)
-;;
-
 let get_locations lines =
+  let get_line_locations re row line =
+    List.map (Re.all re line) ~f:(fun group ->
+      let l, r = (Re.Group.all_offset group).(0) in
+      row, l, r - 1)
+  in
   List.foldi lines ~init:([], []) ~f:(fun row (d_rows, s_rows) line ->
     ( d_rows @ get_line_locations re_digit row line
     , s_rows @ get_line_locations re_symbol row line ))
 ;;
 
-let is_adjacent_row (row, _, _) (row', _, _) = row' >= row - 1 && row' <= row + 1
-let has_overlap (_, l, r) (_, l', r') = l' <= r + 1 && r' >= l - 1
-
 let get_adjacents rows loc =
+  let is_adjacent_row (row, _, _) (row', _, _) = row' >= row - 1 && row' <= row + 1 in
+  let has_overlap (_, l, r) (_, l', r') = l' <= r + 1 && r' >= l - 1 in
   rows |> List.filter ~f:(fun loc' -> is_adjacent_row loc loc' && has_overlap loc loc')
 ;;
 
