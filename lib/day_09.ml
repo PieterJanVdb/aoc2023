5d4ea6d8227188694c698e8e8bb5abb66f10c461
Pieter-Jan Vandenbussche <pj.vandenbussche@gmail.com>
2023-12-09 14:36:55 +0100

    inline fn

--- lib/day_09.ml
+++ lib/day_09.ml
@@ -1,14 +1,14 @@
 open Base
 
 let rec get_next ?(rev = false) seq =
-  let rec get_diffs seq =
-    match seq with
-    | a :: b :: tl -> (b - a) :: get_diffs (b :: tl)
-    | _ -> []
-  in
   if List.for_all seq ~f:(fun n -> n = 0)
   then 0
   else (
+    let rec get_diffs seq =
+      match seq with
+      | a :: b :: tl -> (b - a) :: get_diffs (b :: tl)
+      | _ -> []
+    in
     let next_seq = get_diffs seq in
     let op = if rev then Int.( - ) (List.hd_exn seq) else Int.( + ) (List.last_exn seq) in
     op (get_next next_seq ~rev))
