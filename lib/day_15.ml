66e54b090ab3b52dd17b9e9d15f84ab1bc7803a9
Pieter-Jan Vandenbussche <pj.vandenbussche@gmail.com>
2023-12-15 15:08:44 +0100

    use hashtbl

--- lib/day_15.ml
+++ lib/day_15.ml
@@ -14,30 +14,31 @@ let part1 input =
 ;;
 
 let part2 input =
-  let empty_boxes =
-    Map.of_alist_exn (module Int) (List.init 256 ~f:(fun idx -> idx, []))
+  let boxes = Hashtbl.create (module Int) in
+  let () =
+    input
+    |> String.split ~on:','
+    |> List.iter ~f:(fun instruction ->
+      let label, focal =
+        match String.split ~on:'=' instruction with
+        | [ label; focal ] -> label, Some (Int.of_string (String.strip focal))
+        | _ -> String.drop_suffix instruction 1, None
+      in
+      let box = hash label in
+      let line = Hashtbl.find_or_add boxes box ~default:(fun _ -> []) in
+      let new_line =
+        match focal with
+        | Some focal ->
+          if List.Assoc.mem line ~equal:String.equal label
+          then
+            List.map line ~f:(fun (l, f) ->
+              if String.equal l label then l, focal else l, f)
+          else (label, focal) :: line
+        | None -> List.Assoc.remove line ~equal:String.equal label
+      in
+      Hashtbl.set boxes ~key:box ~data:new_line)
   in
-  input
-  |> String.split ~on:','
-  |> List.fold ~init:empty_boxes ~f:(fun boxes instruction ->
-    let label, focal =
-      match String.split ~on:'=' instruction with
-      | [ label; focal ] -> label, Some (Int.of_string (String.strip focal))
-      | _ -> String.drop_suffix instruction 1, None
-    in
-    let box = hash label in
-    let line = Map.find_exn boxes box in
-    let new_line =
-      match focal with
-      | Some focal ->
-        if List.Assoc.mem line ~equal:String.equal label
-        then
-          List.map line ~f:(fun (l, f) -> if String.equal l label then l, focal else l, f)
-        else (label, focal) :: line
-      | None -> List.Assoc.remove line ~equal:String.equal label
-    in
-    Map.set boxes ~key:box ~data:new_line)
-  |> Map.fold ~init:0 ~f:(fun ~key ~data sum ->
+  Hashtbl.fold boxes ~init:0 ~f:(fun ~key ~data sum ->
     let focusing_powers =
       List.foldi (List.rev data) ~init:0 ~f:(fun idx sum (_, focal) ->
         sum + ((key + 1) * (idx + 1) * focal))
